9771aae7751da0d777918aa0ff568eada02e3ba2
stryczynski <stryczynski15@gmail.com>
2025-10-30 20:23:45 +0100

    Refactor Task class to streamline getter/setter methods and maintain completion logic

--- src/Task.java
+++ src/Task.java
@@ -1,40 +1,19 @@
 public class Task {
-
     private String title;
     private String description;
     private boolean isCompleted;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isCompleted() {
-        return isCompleted;
-    }
-
-    public void setCompleted(boolean completed) {
-        isCompleted = completed;
-    }
-
-    public void complete() {
-        isCompleted = true;
-    }
-
     public Task(String title, String description) {
         this.title = title;
         this.description = description;
-        this.isCompleted = false;
     }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public boolean isCompleted() { return isCompleted; }
+    public void complete() { isCompleted = true; }
 }
